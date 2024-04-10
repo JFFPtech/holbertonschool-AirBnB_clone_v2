7407a3c49344406a702c2c1c78e20bbaaf4caf78
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-10 05:41:39 -0700

    added getenv, models.city to imports in state

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -11,13 +13,11 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
 
-    @property
-    def cities(self):
-        """Getter method to return the list of City objects linked to the current State"""
-        from models import storage
-        city_objs = []
-        cities_dict = storage.all(City)
-        for city in cities_dict.values():
-            if city.state_id == self.id:
-                city_objs.append(city)
-        return city_objs
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Returns the list of City instances with state_id equal to the
+            current State.id"""
+            from models import storage
+            cities = storage.all(City)
+            return [city for city in cities.values() if city.state_id == self.id]
