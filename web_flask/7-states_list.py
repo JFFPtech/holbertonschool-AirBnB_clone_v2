d3f269e5dc58a2390e66d81889dc1d0fd776254c
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-10 05:44:24 -0700

    updated html for states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,21 +2,18 @@
 """ Module to start a Flask web application """
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
 
 
 if __name__ == '__main__':
