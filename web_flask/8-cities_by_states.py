bfbcbdcc82d672413a78d04ccfc4eadf037a1803
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-04-11 08:37:46 -0700

    switchd from query to storage.all

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from models import State, City
+from models import storage
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db'
@@ -18,30 +19,30 @@ class State(db.Model):
 
 # Define the City model
 class City(db.Model):
-    """
-    Represents a city in the database.
-
-    Attributes:
-        id (int): The unique identifier for the city.
-        name (str): The name of the city.
-        state_id (int): The foreign key referencing the associated state.
-    """
     __tablename__ = 'cities'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
     state_id = db.Column(db.Integer, db.ForeignKey('states.id'), nullable=False)
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Renders a template that displays a list of states.
+
+    Returns:
+        The rendered template with the list of states.
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    states = State.query.all()
-    return render_template('8-cities_by_states.html', states=states)
+    cities = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=cities)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    db.session.remove()
+    storage.close()
 
 if __name__ == '__main__':
-    with app.app_context():
-        # It's a good practice to create tables automatically if they don't exist yet.
-        db.create_all()
     app.run(debug=True, host='0.0.0.0')
