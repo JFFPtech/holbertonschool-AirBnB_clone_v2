bdc981fb936e0086b12f16b170440cda42cf0d2b
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-04-12 22:40:14 -0400

    pycode fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,25 +5,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_route(text='is cool'):
     return 'Python %s' % text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_route(n):
     return '%d is a number' % n
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
